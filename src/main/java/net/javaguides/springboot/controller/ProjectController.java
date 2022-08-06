9f0a1683314b5f0d2c428242ab59b15843b0ad94
Pawel Rosiek <rosiu978@gmail.com>
2022-08-06 15:06:58 +0200

    CORS update

--- src/main/java/net/javaguides/springboot/controller/ProjectController.java
+++ src/main/java/net/javaguides/springboot/controller/ProjectController.java
@@ -7,6 +7,7 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,6 +21,7 @@ import net.javaguides.springboot.exception.ResourceNotFoundException;
 import net.javaguides.springboot.model.Project;
 import net.javaguides.springboot.repository.ProjectRepository;
 
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 @RequestMapping("/api/v1/")
 public class ProjectController {
